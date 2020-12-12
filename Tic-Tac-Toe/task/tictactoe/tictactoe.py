1b0b5571fb84e0d93099b3793cb1e4d7d1b9b914
supermads <madisonsfahey@gmail.com>
2020-12-11 17:02:36 -0800

    Stage 5 of Project

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,76 +1,57 @@
 # write your code here
 
-def print_grid(coord_grid):
+def print_grid(grid):
     print("---------")
-    print("| {} {} {} |".format(coord_grid[0][2], coord_grid[1][2], coord_grid[2][2]))
-    print("| {} {} {} |".format(coord_grid[0][1], coord_grid[1][1], coord_grid[2][1]))
-    print("| {} {} {} |".format(coord_grid[0][0], coord_grid[1][0], coord_grid[2][0]))
+    print("| {} {} {} |".format(grid[0][2], grid[1][2], grid[2][2]))
+    print("| {} {} {} |".format(grid[0][1], grid[1][1], grid[2][1]))
+    print("| {} {} {} |".format(grid[0][0], grid[1][0], grid[2][0]))
     print("---------")
-
-
-def make_move(coord_grid):
-    a, b = input("Enter the coordinates: ").split()
-    try:
-        a = int(a)
-        b = int(b)
-    except ValueError:
-        print("You should enter numbers!")
-        return
-    if a < 1 or a > 3 or b < 1 or b > 3:
-        print("Coordinates should be from 1 to 3!")
-        return 
-    elif coord_grid[a - 1][b - 1]:
-        print("This cell is occupied! Choose another one!")
-        return 
-    else:
-        return a - 1, b - 1
 
 
 def find_winner(grid):
     winner = ''
-    acc = 0
-    rows = [[grid[0], grid[1], grid[2]], [grid[3], grid[4], grid[5]], [grid[6], grid[7], grid[8]]]
-    columns = [[grid[0], grid[3], grid[6]], [grid[1], grid[4], grid[7]], [grid[2], grid[5], grid[8]]]
-    diagonals = [[grid[0], grid[4], grid[8]], [grid[1], grid[4], grid[7]], [grid[2], grid[4], grid[6]]]
+    rows = [[grid[0][2], grid[1][2], grid[2][2]], [grid[0][1], grid[1][1], grid[2][1]], [grid[0][0], grid[1][0], grid[2][0]]]
+    columns = [[grid[0][2], grid[0][1], grid[0][0]], [grid[1][2], grid[1][1], grid[1][0]], [grid[2][2], grid[2][1], grid[2][0]]]
+    diagonals = [[grid[0][2], grid[1][1], grid[2][0]], [grid[1][2], grid[1][1], grid[1][0]], [grid[2][2], grid[1][1], grid[0][0]]]
     for row in rows:
         if row[0] == row[1] == row[2]:
             winner = row[0]
-            acc += 1
     for column in columns:
         if column[0] == column[1] == column[2]:
             winner = column[0]
-            acc += 1
     for diagonal in diagonals:
         if diagonal[0] == diagonal[1] == diagonal[2]:
             winner = diagonal[0]
-            acc += 1
-    if acc <= 1:
-        return winner
-    else:
-        return "impossible"
+    return winner
 
 
 def main():
-    grid = list(input("Enter cells: "))
-    coord_grid = [[grid[6], grid[3], grid[0]], [grid[7], grid[4], grid[1]], [grid[8], grid[5], grid[2]]]
-    print_grid(coord_grid)
-    keep_playing = True
-    while keep_playing:
+    grid = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+    print_grid(grid)
+    count = 0
+    while count < 9:
         a, b = input("Enter the coordinates: ").split()
         try:
-            a = int(a)
-            b = int(b)
+            a = int(a) - 1
+            b = int(b) - 1
         except ValueError:
             print("You should enter numbers!")
-        if a < 1 or a > 3 or b < 1 or b > 3:
+        if a < 0 or a > 2 or b < 0 or b > 2:
             print("Coordinates should be from 1 to 3!")
-        elif coord_grid[a - 1][b - 1] == 'X' or coord_grid[a - 1][b - 1] == "O":
+        elif grid[a][b] == 'X' or grid[a][b] == "O":
             print("This cell is occupied! Choose another one!")
         else:
-            coord_grid[a - 1][b - 1] = "X"
-            print_grid(coord_grid)
-            keep_playing = False
-  
+            if count % 2 == 0:
+                grid[a][b] = "X"
+            else:
+                grid[a][b] = "O"
+            count += 1
+            print_grid(grid)
+    winner = find_winner(grid)
+    if not winner:
+        print("Draw")
+    else:
+        print("{} wins".format(winner))
         
 
 main()
