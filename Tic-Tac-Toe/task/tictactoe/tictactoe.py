c7bbce96d6656da0daf8b5f2755848d2e81caa5a
supermads <madisonsfahey@gmail.com>
2020-12-11 17:01:13 -0800

    Stage 4 of Project

--- Tic-Tac-Toe/task/tictactoe/tictactoe.py
+++ Tic-Tac-Toe/task/tictactoe/tictactoe.py
@@ -1,11 +1,29 @@
 # write your code here
 
-def print_grid(grid):
+def print_grid(coord_grid):
     print("---------")
-    print("| {} {} {} |".format(grid[0], grid[1], grid[2]))
-    print("| {} {} {} |".format(grid[3], grid[4], grid[5]))
-    print("| {} {} {} |".format(grid[6], grid[7], grid[8]))
+    print("| {} {} {} |".format(coord_grid[0][2], coord_grid[1][2], coord_grid[2][2]))
+    print("| {} {} {} |".format(coord_grid[0][1], coord_grid[1][1], coord_grid[2][1]))
+    print("| {} {} {} |".format(coord_grid[0][0], coord_grid[1][0], coord_grid[2][0]))
     print("---------")
+
+
+def make_move(coord_grid):
+    a, b = input("Enter the coordinates: ").split()
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print("You should enter numbers!")
+        return
+    if a < 1 or a > 3 or b < 1 or b > 3:
+        print("Coordinates should be from 1 to 3!")
+        return 
+    elif coord_grid[a - 1][b - 1]:
+        print("This cell is occupied! Choose another one!")
+        return 
+    else:
+        return a - 1, b - 1
 
 
 def find_winner(grid):
@@ -34,19 +52,25 @@ def find_winner(grid):
 
 def main():
     grid = list(input("Enter cells: "))
-    print_grid(grid)
-    x_count = grid.count("X")
-    o_count = grid.count("O")
-    winner = find_winner(grid)
-    if x_count >= (o_count + 2) or o_count >= (x_count + 2) or winner == "impossible":
-        print("Impossible")
-    elif not winner:
-        if x_count + o_count == 9:
-            print("Draw")
+    coord_grid = [[grid[6], grid[3], grid[0]], [grid[7], grid[4], grid[1]], [grid[8], grid[5], grid[2]]]
+    print_grid(coord_grid)
+    keep_playing = True
+    while keep_playing:
+        a, b = input("Enter the coordinates: ").split()
+        try:
+            a = int(a)
+            b = int(b)
+        except ValueError:
+            print("You should enter numbers!")
+        if a < 1 or a > 3 or b < 1 or b > 3:
+            print("Coordinates should be from 1 to 3!")
+        elif coord_grid[a - 1][b - 1] == 'X' or coord_grid[a - 1][b - 1] == "O":
+            print("This cell is occupied! Choose another one!")
         else:
-            print("Game not finished")
-    else:
-        print("{} wins".format(winner))
+            coord_grid[a - 1][b - 1] = "X"
+            print_grid(coord_grid)
+            keep_playing = False
+  
         
 
 main()
